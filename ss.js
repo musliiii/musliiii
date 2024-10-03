cb7e90bb3203a9821d97caa2f9052e9a38830f33
musliiii <xbox270703@gmail.com>
2024-10-03 18:39:37 +0400

    ss.js

---
+++ ss.js
@@ -0,0 +1,13 @@
+// Создаем объект "Пользователь форума"
+let user = {
+    username: 'JohnDoe',
+    email: 'johndoe@gmail.com',
+    age: 30,
+    isAdmin: false
+  };
+  
+  // Выводим информацию о пользователе с помощью alert
+  alert(`Имя пользователя: ${user.username} 
+  Email: ${user.email}
+  Возраст: ${user.age}
+  Администратор: ${user.isAdmin ? 'Да' : 'Нет'}`);
